44e30aa18044be51c07ce6eed30052fc7f6d519e
mallikmusaddiq1 <mallikmusaddiq1@gmail.com>
2025-07-04 14:57:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
 setup(
     name="yt-chap",
     version="0.1.0",
+    description="CLI tool to display YouTube chapters using yt-dlp",
     author="Mallik Mohammad Musaddiq",
     author_email="mallikmusaddiq1@gmail.com",
-    description="CLI tool to view YouTube video chapters in human-readable format",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/mallikmusaddiq1/yt-chap",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Environment :: Console",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    install_requires=[
-        "yt-dlp",  # ✅ Automatically fetches latest compatible yt-dlp
-    ],
     entry_points={
         "console_scripts": [
-            "yt-chap = yt_chap.cli:main",
+            "yt-chap=yt_chap.cli:main"
         ]
     },
-    include_package_data=True,
-    license="MIT",
+    install_requires=[
+        yt-dlp,
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.6",
 )
